5d38db2a6c5d7cc037423e6017a557a31dfcbd80
Aleix D <dominguez.aleix@apfutura.com>
2024-09-24 22:02:50 +0200

    files to deploy vercel

--- web_lliga/settings.py
+++ web_lliga/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^n700!j=*4%&d-2%@(t9eg&i09=8(5_z+z(i!txq&hft^8y+ch
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
