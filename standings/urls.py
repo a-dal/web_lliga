516e327d7a8c543f678671b7538b181f9fbdcbee
Aleix D <dominguez.aleix@apfutura.com>
2024-09-25 00:40:47 +0200

    fix url tornejos

--- standings/urls.py
+++ standings/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='standings-home'),
-    path('/tornejos', views.tornejos, name='standings-tornejos'),
+    path('tornejos', views.tornejos, name='standings-tornejos'),
 ]
