9d7027f3d8babd7156ce83e1c0f87c78212b70ac
Aleix D <dominguez.aleix@apfutura.com>
2024-11-12 22:46:50 +0100

    Jornada 7

--- standings/views.py
+++ standings/views.py
@@ -81,16 +81,14 @@ rounds = [
     {
         "number": "7",
         "players": [
-            {"name": "", "points": 10, "deck":""},
-            {"name": "", "points": 9, "deck":""},
-            {"name": "", "points": 8, "deck":""},
-            {"name": "", "points": 7, "deck":""},
-            {"name": "", "points": 6, "deck":""},
-            {"name": "", "points": 5, "deck":""},
-            {"name": "", "points": 4, "deck":""},
-            {"name": "", "points": 3, "deck":""},
-            {"name": "", "points": 2, "deck":""},
-            {"name": "", "points": 1, "deck":""}
+            {"name": "Aleix Dominguez", "points": 10, "deck":"Phlage, Titan of Fire's Fury", "icons": ["w", "r"]},
+            {"name": "Beto Berdegal", "points": 9, "deck":"Ghyrson Starn, Kelermorph", "icons": ["r", "u"]},
+            {"name": "Alex Garcia", "points": 8, "deck":"Basim Ibn Ishaq", "icons": ["u", "b"]},
+            {"name": "Joan Lloret", "points": 7, "deck":"Phelia, Exuberant Shepherd", "icons": ["w"]},
+            {"name": "Carlos Pedrosa", "points": 6, "deck":"Ghyrson Starn, Kelermorph", "icons": ["r", "u"]},
+            {"name": "Sergi Codina", "points": 5, "deck":"Gev, Scaled Scorch", "icons": ["b", "r"]},
+            {"name": "Martí Llinés", "points": 4, "deck":"Emmara, Soul of the Accord", "icons": ["g", "w"]},
+            {"name": "Eros Montero", "points": 3, "deck":"Phelia, Exuberant Shepherd", "icons": ["w"]}
         ]
     },
     {
